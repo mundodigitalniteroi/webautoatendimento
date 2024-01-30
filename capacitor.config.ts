b37bee171b4e6db4f938d4b3748a76272e477ced
Leonardo Oliveira <leolirarj@yahoo.com.br>
2024-01-30 14:23:27 -0300

    ajustes de nome

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
-  appId: "io.ionic.demo.pg.cap.ng",
-  appName: "Photo Gallery Cap Ng",
+  appId: 'autoatendimento.patiosg.com.br',
+  appName: 'Auto Atendimento',
   bundledWebRuntime: false,
-  npmClient: "npm",
-  webDir: "www",
+  npmClient: 'npm',
+  webDir: 'www',
 };
 
 export default config;
