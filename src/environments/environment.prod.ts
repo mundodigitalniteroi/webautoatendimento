f0428b75ce3b429cf9978ab6234701236ab609c8
guilhermedel <glhrmgithub@gmail.com>
2025-05-19 16:54:16 -0300

    Atualiza credenciais de integração com a API da SumUp no ambiente de produção. As variáveis merchantCode, clientId e clientSecret foram alteradas para novos valores, garantindo a continuidade do funcionamento do sistema. Essa mudança é essencial para a manutenção da segurança e funcionalidade da aplicação.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,9 +6,9 @@ export const environment = {
   // urlApiDP: 'https://api.autoatendimento.webzi.com.br',
   urlApiSumup: 'https://api.sumup.com',
   urlApiWebziPay:'https://api.webzi.com.br',
-  merchantCode: 'MDEUZAGC',
-  clientId: 'cc_classic_kWwcKzywkLEZcOM9yEOpSrExMnnQl',
-  clientSecret: 'cc_sk_classic_WHizzlncLknRFfNnRHFEukUuwX9j6xD2BMqqcyS3R6RNNuU7p8',
+  merchantCode: 'MXHHSFK3',
+  clientId: 'cc_classic_Kkmv1mybdQfj6PKrLjTVzEjV1TV69',
+  clientSecret: 'cc_sk_classic_Nf329UVAb4pbl5YtH3QKEsuy5uvP8BvHDvgLG6Csq42nJzxPQl',
   redirectUri: 'sumupmobile://callback',
   versao: '1.2.2',
 };
