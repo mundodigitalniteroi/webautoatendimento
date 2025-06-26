43e49373a9ceeb1d99df4c4eaac1f8370bedc22b
guilhermedel <glhrmgithub@gmail.com>
2025-06-26 17:39:18 -0300

    Atualiza URL da API de atendimento para o ambiente de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlApiAtendimento: 'https://api.atendimento.gestordepatios.app.br',
+  urlApiAtendimento: 'https://api.atendimentodetranal.gestordepatios.app.br',
   urlApiDP: 'https://api.autoatendimento.webzi.com.br',
   versao: '1.2.2',
 };
