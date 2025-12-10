4775bf2a4d6769ff3ce4c46baec44153cace3956
Leonardo Oliveira <leolirarj@yahoo.com.br>
2025-12-10 19:12:11 -0300

    ajustes para deposito chapeco

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   urlApiAtendimento: 'https://localhost:7228',
-  urlApiDP: 'https://localhost:7093',
+  urlApiDP: 'https://api.autoatendimento.webzi.com.br',
   versao: '1.0.0',
 };
